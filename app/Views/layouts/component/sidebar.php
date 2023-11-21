d2797ddcdae0174a47cf0f49ac1eeae874ee7de3
alfyadinata <alfymuhammad7@gmail.com>
2023-11-21 22:55:50 +0700

    setup template ui

---
+++ app/Views/layouts/component/sidebar.php
@@ -0,0 +1,41 @@
+<div class="sidebar">
+    <nav class="sidebar-nav">
+        <ul class="nav">
+            <li class="nav-item">
+                <a class="nav-link" href="index.html">
+                    <i class="nav-icon icon-speedometer"></i> Dashboard
+                    <span class="badge badge-primary">NEW</span>
+                </a>
+            </li>
+            <li class="nav-title">Master</li>
+            <li class="nav-item">
+                <a class="nav-link" href="<?= site_url('/stocks'); ?>">
+                    <i class="nav-icon icon-drop"></i> Barang
+                </a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="colors.html">
+                    <i class="nav-icon icon-drop"></i> Lokasi
+                </a>
+            </li>
+            <li class="nav-title">Components</li>
+            <li class="nav-item nav-dropdown">
+                <a class="nav-link nav-dropdown-toggle" href="#">
+                    <i class="nav-icon icon-puzzle"></i> Base</a>
+                <ul class="nav-dropdown-items">
+                    <li class="nav-item">
+                        <a class="nav-link" href="base-breadcrumb.html">
+                            <i class="nav-icon icon-puzzle"></i> Breadcrumb</a>
+                    </li>
+                </ul>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link nav-link-danger" href="https://coreui.io/pro/" target="_top">
+                    <i class="nav-icon icon-layers"></i> Try CoreUI
+                    <strong>PRO</strong>
+                </a>
+            </li>
+        </ul>
+    </nav>
+    <button class="sidebar-minimizer brand-minimizer" type="button"></button>
+</div>
---
+++ app/Views/layouts/main.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+
+  <?php echo $this->renderSection('title'); ?>
+
+  <!-- Icons-->
+  <link href="<?php echo base_url('core-ui/vendors/@coreui/icons/css/coreui-icons.min.css'); ?>" rel="stylesheet">
+  <link href="<?php echo base_url('core-ui/vendors/flag-icon-css/css/flag-icon.min.css'); ?>" rel="stylesheet">
+  <link href="<?php echo base_url('core-ui/vendors/font-awesome/css/font-awesome.min.css'); ?>" rel="stylesheet">
+  <link href="<?php echo base_url('core-ui/vendors/simple-line-icons/css/simple-line-icons.css'); ?>" rel="stylesheet">
+  <!-- Main styles for this application-->
+  <link href="<?php echo base_url('core-ui/css/style.css'); ?>" rel="stylesheet">
+  <link href="<?php echo base_url('core-ui/vendors/pace-progress/css/pace.min.css'); ?>" rel="stylesheet">
+</head>
+
+<body class="app header-fixed sidebar-fixed aside-menu-fixed sidebar-lg-show">
+  <!-- Navbar -->
+  <?php echo $this->include('layouts/component/navbar'); ?>
+  <!-- end of navbar -->
+  <div class="app-body">
+    <!-- Sidebar -->
+    <?php echo $this->include('layouts/component/sidebar'); ?>
+    <!-- end of Sidebar -->
+
+    <!-- Main Content -->
+    <?php echo $this->renderSection('content'); ?>
+
+
+    <!-- End of Main Content -->
+
+
+    <?php echo $this->include('layouts/component/aside-menu'); ?>
+
+  </div>
+  <footer class="app-footer">
+    <div>
+      <a href="https://coreui.io">CoreUI</a>
+      <span>&copy; 2018 creativeLabs.</span>
+    </div>
+    <div class="ml-auto">
+      <span>Powered by</span>
+      <a href="https://coreui.io">CoreUI</a>
+    </div>
+  </footer>
+  <!-- Bootstrap and necessary plugins-->
+  <script src="<?php echo base_url('core-ui/vendors/jquery/js/jquery.min.js'); ?>"></script>
+  <script src="<?php echo base_url('core-ui/vendors/popper.js/js/popper.min.js'); ?>"></script>
+  <script src="<?php echo base_url('core-ui/vendors/bootstrap/js/bootstrap.min.js'); ?>"></script>
+  <script src="<?php echo base_url('core-ui/vendors/pace-progress/js/pace.min.js'); ?>"></script>
+  <script src="<?php echo base_url('core-ui/vendors/perfect-scrollbar/js/perfect-scrollbar.min.js'); ?>"></script>
+  <script src="<?php echo base_url('core-ui/vendors/@coreui/coreui/js/coreui.min.js'); ?>"></script>
+  <!-- Plugins and scripts required by this view-->
+  <script src="<?php echo base_url('core-ui/vendors/chart.js/js/Chart.min.js'); ?>"></script>
+  <script src="<?php echo base_url('core-ui/vendors/@coreui/coreui-plugin-chartjs-custom-tooltips/js/custom-tooltips.min.js'); ?>"></script>
+  <script src="<?php echo base_url('core-ui/js/main.js'); ?>"></script>
+
+  <?php echo $this->renderSection('extra-js'); ?>
+</body>
+
+</html>
